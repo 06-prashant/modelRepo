836d87f90080356b5d0342d5d995405173a369e1
Prashant Srivastava <52559070+06-prashant@users.noreply.github.com>
2023-01-17 13:25:42 +0530

    9th commit

--- myFile.js
+++ myFile.js
@@ -6,7 +6,7 @@ const os= require('os');
 
 // console.log(exports, require, module, __filename, __dirname);
 console.log(os.freemem());
-// console.log(os.homedir());
+console.log(os.homedir());
 console.log(os.hostname());
 console.log(os.release());
 console.log(os.platform());
