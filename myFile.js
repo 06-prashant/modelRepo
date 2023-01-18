b4a91539ce7e692d4912afdb5450fbc440b22829
Prashant Srivastava <52559070+06-prashant@users.noreply.github.com>
2023-01-18 12:19:34 +0530

    9th commit

--- myFile.js
+++ myFile.js
@@ -1,12 +1,23 @@
 const pras=require("./my2");
 const os= require('os');
-
+const fs=require('fs');
+// const file=require("./file");
 // console.log(pras);
 // document.getElementById("p1").innerHTML="HelloV";
 
 // console.log(exports, require, module, __filename, __dirname);
-console.log(os.freemem());
-console.log(os.homedir());
-console.log(os.hostname());
-console.log(os.release());
-console.log(os.platform());
+// console.log(os.freemem());
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.release());
+// console.log(os.platform());
+
+// fs.readFile('file.txt', 'utf8', (err, text)=>{
+//     console.log(err, text)
+// })
+
+
+// const c= fs.readFileSync('file.txt')
+// console.log(c.toString());
+
+const c= fs.writeFileSync('file.txt', "AbaJabaJhaba")
